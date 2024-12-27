dec8842b4f17d310370ad9290ff8bcd93e15b53b
XBold <80431693+XBold@users.noreply.github.com>
2024-12-27 18:55:32 +0100

    - Fix date and time on logger - First stable release

---
+++ Classes/Logger.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualBasic;
+
+namespace Tools.Classes
+{
+    public static class Logger
+    {
+        /// <summary>
+        /// Add a line of log with the specified message
+        /// </summary>
+        /// <param name="message">Message to insert inside log</param>
+        /// <param name="severity">Severity to use 0 = [INFO], 1 = [WARNING], 2 = [CRITICAL], 3..255 = [FATAL ERROR]</param>
+        /// <param name="numberOfMilliseconds">Number of milliseconds to use in the date and time. 0 = NO DATE AND TIME. Parameter not specified = 2 millseconds unit</param>
+        public static void Log(string message, byte severity, byte numberOfMilliseconds = 3)
+        {
+            Console.WriteLine(CreateLog(message, severity, numberOfMilliseconds));
+        }
+
+        /// <summary>
+        /// Add a line of log with the specified message to the specified file
+        /// </summary>
+        /// <param name="message">Message to insert inside log</param>
+        /// <param name="severity">Severity to use 0 = [INFO], 1 = [WARNING], 2 = [CRITICAL], 3..255 = [FATAL ERROR]</param>
+        /// <param name="filePathAndName">Path and file name where to save log file. Parameters not specified = log.txt, same folder where program it's executed</param>
+        /// <param name="numberOfMilliseconds">Number of milliseconds to use in the date and time. 0 = NO DATE AND TIME. Parameter not specified = 2 millseconds unit</param>
+        public static void Log(string message, byte severity, string filePathAndName, byte numberOfMilliseconds = 3)
+        {
+            File.AppendAllText(filePathAndName, CreateLog(message, severity, numberOfMilliseconds));
+        }
+
+        private static string CreateLog(string message, byte severity, byte numberOfMilliseconds = 3)
+        {
+            string format = "dd-MM-yyyy HH:mm:ss:" + new string('f', General.Limit(numberOfMilliseconds, 1, 7));
+            format += " ";
+            string sev = severity == 0 ? "[INFO]" : severity == 1 ? "[WARNING]" : severity == 2 ? "[CRITICAL]" : "[FATAL ERROR]";
+            sev += " ";
+            return sev + (numberOfMilliseconds > 0 ? DateAndTime.Now.ToString(format) : "") + message;
+        }
+    }
+}
---
+++ General.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Tools
+{
+    public static partial class General
+    {
+        [GeneratedRegex("[^0-9.]+")]
+        private static partial Regex NumericAndDots();
+        [GeneratedRegex("[^0-9]+")]
+        private static partial Regex Numeric();
+
+        /// <summary>
+        /// Limit the value between the MIN parameter and the MAX parameter
+        /// </summary>
+        public static T Limit<T>(T value, T min, T max) where T : IComparable<T>
+        {
+            return (value.CompareTo(min) < 0) ? min : (value.CompareTo(max) > 0) ? max : value;
+        }
+
+        /// <summary>
+        /// Check that the input text contains only number or number with dots
+        /// </summary>
+        /// <param name="acceptDots">TRUE -> The input text can containt numbers AND dots</param>
+        public static bool IsTextNumeric(string text, bool acceptDots = false)
+        {
+            if (acceptDots)
+            {
+                Regex regex = NumericAndDots();
+                return !regex.IsMatch(text);
+            }
+            else
+            {
+                Regex regex = Numeric();
+                return !regex.IsMatch(text);
+            }
+        }
+
+    }
+}
